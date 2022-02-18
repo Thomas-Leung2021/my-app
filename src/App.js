d9cf9cbad144e72a344e96ad562ae6853b987ffb
Thomas-Leung2021 <thomas.leung@netexperience.com>
2022-02-17 23:31:26 -0500

    added onclick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import Navbar from "./components/Navbar"
-import Card from "./components/Card"
-import data from "./data" // .js by default
+import React from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Main from "./components/Main";
+import data from "./data"; // .js by default
 
 function App() {
   // we need a unique key prop when using map()
-  const cards = data.map(item => {
-    return (<Card key={item.key} id={item.id} name={item.fullName} email={item.email}/>);
-  })
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.key}
+        id={item.id}
+        name={item.fullName}
+        email={item.email}
+      />
+      // Short hand for above using object literal
+      // <Card key={item.key} {...item} />
+      // alternative, but your need to use prop.item.fullName instead of prop.name
+      //   <Card
+      //   key={item.key}
+      //   item={item}
+      // />
+    );
+  });
   return (
-    <div className="container"><Navbar /><main>{cards}</main></div>
+    <div className="container">
+      <Navbar />
+      <Main/>
+      <main>{cards}</main>
+    </div>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Main() {
   return (
     <>
-      <main>Hello</main>
+      <main>
+        <div className="form">
+          <input className="form--input" placeholder="Top text" type="text" />
+          <input className="form--input" placeholder="Bottom text" type="text" />
+          <button className="form--button">Get a new meme image</button>
+        </div>
+      </main>
     </>
   );
 }
